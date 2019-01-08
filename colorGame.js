5e9d0942dd0b9e2c79c4c576d5a4379d473a33d8
TJ Jackson <elitefourfresh@gmail.com>
2019-01-08 14:51:20 -0500

    RANDOM RGB

--- colorGame.js
+++ colorGame.js
@@ -7,8 +7,9 @@ var colors = [
     "rgb(255, 0, 255)",
 ]
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -22,9 +23,26 @@ for(var i = 0; i < squares.length; i++) {
         var clickedColor = this.style.backgroundColor;
         //compare color to pickedColor
         if(clickedColor === pickedColor) {
-            alert("Correct!");
+            messageDisplay.textContent = "Correct!";
+            changeColors(clickedColor);
         } else {
-            alert("Wrong.");
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again";
         }
     });
+}
+
+function changeColors(color) {
+    //loop through all squares
+    for(var i = 0; i < squares.length; i++) {
+        //change each color to match given color
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor() {
+    //pick random number
+    var random = Math.floor(Math.random() * colors.length)
+    //use number to access index out of colors
+    return colors[random];
 }
